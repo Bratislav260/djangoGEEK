83acb75e64db70d7f41d83ce7387d790b9d048a1
bratislav260 <askarbekov.bakay@gmail.com>
2024-09-12 17:12:12 +0300

    second2 commit

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(null=True, blank=True)
     rate = models.IntegerField(default=0)
+    creates_at = models.DateTimeField(auto_created=True, null=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title}"
